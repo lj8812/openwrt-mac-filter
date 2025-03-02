ffc04e1be409f4038131c9ba6c40cf9c02e29fc7
lj8812 <74142076+lj8812@users.noreply.github.com>
2025-03-02 21:03:24 +0800

    Update mac-filter.lua

--- luasrc/controller/mac-filter.lua
+++ luasrc/controller/mac-filter.lua
@@ -1,5 +1,5 @@
 module("luci.controller.mac-filter", package.seeall)
 
 function index()
-    entry({"admin", "services", "mac-filter"}, cbi("mac-filter"), _("MAC Filtering"), 60)
+    entry({"admin", "services", "mac-filter"}, cbi("mac-filter"), _("MAC Filter"), 60)
 end
